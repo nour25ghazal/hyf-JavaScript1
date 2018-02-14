1cdb4d6fc61b1ac2b5873e4f5cdf4a23993b2899
Nour ghazal <35364308+nour25ghazal@users.noreply.github.com>
2018-02-14 20:33:20 +0100

    array

---
+++ step6.js
@@ -0,0 +1,7 @@
+const cars = [];
+console.log("the value of my array will be: empty");
+console.log(cars);
+const noursFavAnimals = ["cat", " dog", " hamster", " horse"];
+console.log(noursFavAnimals);
+noursFavAnimals.push("baby pig")
+console.log("nour's and Daan's favorite animals are: " + noursFavAnimals)
