133f0b4f2a7f37edd0f221fdb8804094d4fff3bb
Nour ghazal <35364308+nour25ghazal@users.noreply.github.com>
2018-02-15 03:36:26 +0100

    Delete step1.js

--- step1.js
+++
@@ -1,8 +0,0 @@
-let en = 'hello, world // english';
-let fr = 'salut, mond // french';
-let tr = 'merhaba, dunya // turkish';
-let nl = 'hallo, wereld // dutch';
-console.log(en);
-console.log(fr);
-console.log(tr);
-console.log(nl);
