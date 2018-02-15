fc77abcdcfde6cd0dbdf86d6d0acad396b45fb2c
Nour ghazal <35364308+nour25ghazal@users.noreply.github.com>
2018-02-15 03:33:28 +0100

    Declare a variable and assign a string to it

---
+++ week2/step4.js
@@ -0,0 +1,6 @@
+let y = "hello world";
+console.log("The value of my string will be: 'hello world'");
+console.log(y);
+y = "i know what am doing!";
+console.log("The new assigned value of my string is: 'i know what am doing!'");
+console.log(y);
